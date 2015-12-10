933314977ca52ccbdd0f9141e75e96136b630c22
MSU CMPS <CMPS@bo105-lab-4988.labs.msu>
2015-12-10 13:36:56 -0600

    Add 2D-Tree.py, output.txt, and answer questions

---
+++ 2D-Tree.py
@@ -0,0 +1,155 @@
+import random
+
+class TreeNode:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+        self.p = None
+        self.level = None
+
+    def __repr__(self):
+        return "< Key=%s Parent=%s Level=%s>" % (self.key,self.p, self.level)
+
+    def __str__(self):
+        return "< Key=%s Parent=%s Level=%s>" % (self.key,self.p, self.level)
+
+class KDTree:
+    def __init__(self, dimensions):
+        self.root = None
+        self.dim = dimensions
+
+    def length(self):
+        return self.size
+
+    def inorder(self, node):
+        if node == None:
+            return None
+        else:
+            self.inorder(node.left)
+            print node.key,
+            self.inorder(node.right)
+
+    def search(self, values):
+        disc = 0
+        node = self.root
+        while node != None:
+            print 'Looking at', node
+            if node.key == values:
+                return node
+            if node.key[disc] > values[disc]:
+                node = node.left
+            else:
+                node = node.right
+            disc = (disc+1)%self.dim
+        return None
+
+    def minimum(self, node):
+        x = None
+        while node.left != None:
+            x = node.left
+            node = node.left
+        return x
+
+    def maximum(self, node):
+        x = None
+        while node.right != None:
+            x = node.right
+            node = node.right
+        return x
+
+    def successor(self, node):
+        parent = None
+        if node.right != None:
+            return self.minimum(node.right)
+        parent = node.p
+        while parent != None and node == parent.right:
+            node = parent
+            parent = parent.p
+        return parent
+
+    def predecessor(self, node):
+        parent = None
+        if node.left != None:
+            return self.maximum(node.left)
+        parent = node.p
+        while parent != None and node == parent.left:
+            node = parent
+            parent = parent.p
+        return parent
+
+    def insert(self, values):
+        level = 0
+        disc = 0
+        t = TreeNode(values)
+        parent = None
+        node = self.root
+        while node != None:
+            parent = node
+            if node.key[disc] > t.key[disc]:
+                node = node.left
+            elif node.key[disc] < t.key[disc]:
+                node = node.right
+            else:
+                print 'Node already in the tree'
+            if node == None:
+                disc = (disc+1)%self.dim
+            level+=1
+        t.level = level
+        t.p = parent
+        if parent == None:
+            self.root = t
+        elif t.key[disc] < parent.key[disc]:
+            parent.left = t
+        else:
+            parent.right = t
+        return t
+
+
+    def delete(self, node):
+        if node.left == None:
+            self.transplant(node, node.right)
+        elif node.right == None:
+            self.transplant(node, node.left)
+        else:
+            succ = self.minimum(node.right)
+            if succ.p != node:
+                self.transplant(succ, succ.right)
+                succ.right = node.right
+                succ.right.p = succ
+            self.transplant(node, succ)
+            succ.left = node.left
+            succ.left.p = succ
+
+    def transplant(self, node, newnode):
+        if node.p == None:
+            self.root = newnode
+        elif node == node.p.left:
+            node.p.left = newnode
+        else:
+            node.p.right = newnode
+        if newnode != None:
+            newnode.p = node.p
+
+if __name__ == "__main__":
+
+    insertvals = ([11,11],[22,22],[33,33],[44,44],[55,55],[66,66],[77,77],[88,88],[99,99],[100,100])
+    K = KDTree(2)
+    for i in range(10):
+        K.insert([random.randint(1,100), random.randint(1,100)])
+    for value in insertvals:
+        K.insert(value)
+    search = K.search([100,100])
+    if search:
+        print 'Found it!', search
+    else:
+        print "Didn't find it"
+    """If I wanted to insert pairs of numbers"""
+    """
+    for i in range(10):
+        for j in range(10):
+            r1 = random.randint(1,100)
+            r2 = random.randint(1,100)
+            B.insert([r1,r2])
+            print (r1,r2)
+   """
--- README.md
+++ README.md
@@ -5,10 +5,10 @@ Fall 2015
 
 Take the binary tree implementation provided in this repo and convert it into a 2D-Tree. Use the same
 method of insertion that a KD-Tree would use by assigning a discriminator at each level, but don't worry
-about any data beyond two dimensions. The binary tree implementation was found on some random stack overflow 
+about any data beyond two dimensions. The binary tree implementation was found on some random stack overflow
 post, but it's a pretty thorough implementation. I didn't write it, so I didn't comment the code.
 
-- Provided in the repository is a copy of the original paper for you to reference. 
+- Provided in the repository is a copy of the original paper for you to reference.
 - Also, you may use the internet to assist with your python questions but you may ONLY visit: https://docs.python.org !! If I see you on another site, you will be given a zero.
 
 ### Instructions
@@ -31,72 +31,72 @@ Remember this was my first attempt at teaching this class, so your not gonna hur
 worry about that anyway! Give feedback please.
 
  1. Was python a good language for this course? Would you like to use another, or a mix?
- 
+
  ```
  Answer:
- 
- 
+ Yes, python worked fine for the course.
+
  ```
- 
- 
- 
+
+
+
  2. Should I have concentrated more on the "data structures" and less on writing programs to use the structures? Meaning should I have made an attempt to add to the 4 main structures we covered.
- 
+
   ```
  Answer:
- 
- 
+I would have liked to have had more usage of the data structures over mostly just talking about them.
+
  ```
- 
- 
- 
+
+
+
  3. Were there enough? Or not enough programs for the semester?
- 
- 
+
+
   ```
  Answer:
- 
- 
+ The number of programs wasn't a problem, the problem was the jumping around and not getting very deep into any topics
+
  ```
- 
- 
- 
+
+
+
  4. I already now I should grade programs faster so you don't need to comment on that. I also know my knowledge of the subject matter could be better. I plan on working on both.
- 
+
   ```
  Answer:
- 
- 
+
+
  ```
- 
- 
- 
+
+
+
  5. Did you turn in all your programs? If not why, and how many did you get working?
- 
- 
+
+
   ```
  Answer:
- 
- 
+ Yes
+
  ```
- 
- 
- 
- 6. What is your assumed grade in this course? Meaning based on what you completed, what do you think you earned. 
- 
- 
+
+
+
+ 6. What is your assumed grade in this course? Meaning based on what you completed, what do you think you earned.
+
+
   ```
  Answer:
- 
- 
+ An A
+
  ```
- 
- 
- 
+
+
+
  7. Is this the same, or different than what you thought your grade would be at the start of the semester?
 
  ```
  Answer:
- 
- 
+ The same
+
  ```
